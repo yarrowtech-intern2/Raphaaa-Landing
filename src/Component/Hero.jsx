30676216ad827af43b606c6330634056e002f038
Rahul Panja <rpanja149@gmail.com>
2026-04-21 12:07:32 +0530

    fixed error

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -73,7 +73,7 @@ export default function Hero() {
             <div
               className="
                 relative w-full shadow-md overflow-hidden
-                rounded-[28px] lg:rounded-l-none lg:rounded-r-[999px]
+                rounded-full lg:rounded-l-none lg:rounded-r-[999px]
                 h-[260px] sm:h-[420px] lg:h-[520px]
               "
             >
@@ -87,7 +87,7 @@ export default function Hero() {
               <div
                 className="
                   absolute inset-0 pointer-events-none
-                  rounded-[28px] lg:rounded-l-none lg:rounded-r-[999px]
+                  rounded-full lg:rounded-l-none lg:rounded-r-[999px]
                   border-[7px] sm:border-[10px] border-white
                 "
               />
